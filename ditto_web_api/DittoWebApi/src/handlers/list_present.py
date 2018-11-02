511ecb563c9ace1c4591ce7e724c201bb238f48f
Robert Clegg <Robert.Clegg@tessella.com>
2018-11-02 15:48:27 +0000

    Fix pylint warnings

--- ditto_web_api/DittoWebApi/src/handlers/list_present.py
+++ ditto_web_api/DittoWebApi/src/handlers/list_present.py
@@ -1,11 +1,12 @@
-import tornado.web
+# pylint: disable=W0221,W0223
 import json
+import tornado.web
 
 
 class ListPresentHandler(tornado.web.RequestHandler):
     def initialize(self, data_replication_service):
         self._data_replication_service = data_replication_service
 
-    def get(self):
+    def get(self, *args, **kwargs):
         object_dicts = self._data_replication_service.retrieve_object_dicts()
         self.write(json.dumps(object_dicts))
