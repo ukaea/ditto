b398a24aa873de935ad1595a0b9601c0f8587431
Oliver Tarrant <oliver.tarrant@tessella.com>
2018-11-02 12:43:41 +0000

    Update unit tests

--- ditto_web_api/DittoWebApi/tests/models/objects_test.py
+++ ditto_web_api/DittoWebApi/tests/models/objects_test.py
@@ -9,6 +9,7 @@ class TestObjects(unittest.TestCase):
 
 
     def test_minio_object_is_correctly_converted_to_dictionary(self):
+        # Arrange
         mock_minio_object = mock.Mock()
         mock_minio_object.object_name = "test_1.txt"
         mock_minio_object.bucket_name = "bucket_1_test"
@@ -16,10 +17,10 @@ class TestObjects(unittest.TestCase):
         mock_minio_object.size = 100
         mock_minio_object.etag = "test_etag"
         mock_minio_object.last_modified = datetime.datetime(2018, 11, 15)
-
         test_object = Object(mock_minio_object)
-
+        # Act
         output = test_object.to_dict()
-        assert output == {'object_name': 'test_1.txt', 'bucket_name': 'bucket_1_test', 'is_dir': False, 'size': 100, 'etag': 'test_etag',
-         'last modified': "15/11/2018"}
+        # Assert
+        assert output == {'object_name': 'test_1.txt', 'bucket_name': 'bucket_1_test', 'is_dir': False, 'size': 100,
+                          'etag': 'test_etag', 'last modified': "15/11/2018"}
         self.assertIsInstance(output, dict)
