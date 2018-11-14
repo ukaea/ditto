821295a491a5f0ca9411bdb414ff1625427b059c
Oliver Tarrant <oliver.tarrant@tessella.com>
2018-11-14 16:04:30 +0000

    Test storage diff processor deals with windows files

--- ditto_web_api/DittoWebApi/tests/services/storage_difference_processor_test.py
+++ ditto_web_api/DittoWebApi/tests/services/storage_difference_processor_test.py
@@ -16,18 +16,18 @@ class TestSorageDifferenceProcessor:
         self.file_3 = mock.create_autospec(FileInformation)
         self.file_4 = mock.create_autospec(FileInformation)
         self.file_1.rel_path = "file_1.txt"
-        self.file_2.rel_path = "file_2.txt"
-        self.file_3.rel_path = "file_3.txt"
-        self.file_4.rel_path = "file_4.txt"
+        self.file_2.rel_path = "some_dir/file_2.txt"
+        self.file_3.rel_path = r"some_win_dir\some_win_sub_dir\file_3.txt"
+        self.file_4.rel_path = r"some/badly\formed/path\file_4.txt"
         # Mock s3 objects
         self.s3_object_1 = mock.create_autospec(S3ObjectInformation)
         self.s3_object_2 = mock.create_autospec(S3ObjectInformation)
         self.s3_object_3 = mock.create_autospec(S3ObjectInformation)
         self.s3_object_4 = mock.create_autospec(S3ObjectInformation)
         self.s3_object_1.object_name = "file_1.txt"
-        self.s3_object_2.object_name = "file_2.txt"
-        self.s3_object_3.object_name = "file_3.txt"
-        self.s3_object_4.object_name = "file_4.txt"
+        self.s3_object_2.object_name = "some_dir/file_2.txt"
+        self.s3_object_3.object_name = "some_win_dir/some_win_sub_dir/file_3.txt"
+        self.s3_object_4.object_name = "some/badly/formed/path/file_4.txt"
 
     def test_new_files_returns_all_files_when_only_new_files_are_present(self):
         # Arrange
@@ -37,8 +37,8 @@ class TestSorageDifferenceProcessor:
         result = self.processor.return_new_files(objects_in_bucket, files_in_directory)
         # Assert
         assert len(result) == 2
-        assert result[0].rel_path == "file_1.txt"
-        assert result[1].rel_path == "file_2.txt"
+        assert result[0] == self.file_1
+        assert result[1] == self.file_2
 
     def test_new_files_returns_empty_array_when_all_files_are_already_present(self):
         # Arrange
@@ -49,7 +49,7 @@ class TestSorageDifferenceProcessor:
         # Assert
         assert result == []
 
-    def test_new_files_returns_only_new_files_when_some_are_new_and_some_are_already_present(self):
+    def test_new_files_returns_only_new_files_when_some_are_new_some_are_present_including_non_unix_paths(self):
         # Arrange
         objects_in_bucket = [self.s3_object_2]
         files_in_directory = [self.file_1, self.file_2, self.file_3]
@@ -57,8 +57,8 @@ class TestSorageDifferenceProcessor:
         result = self.processor.return_new_files(objects_in_bucket, files_in_directory)
         # Assert
         assert len(result) == 2
-        assert result[0].rel_path == "file_1.txt"
-        assert result[1].rel_path == "file_3.txt"
+        assert result[0] == self.file_1
+        assert result[1] == self.file_3
 
     def test_new_files_returns_only_new_files_when_there_is_extra_file_in_bucket_but_not_in_dir(self):
         # Arrange
@@ -68,8 +68,8 @@ class TestSorageDifferenceProcessor:
         result = self.processor.return_new_files(objects_in_bucket, files_in_directory)
         # Assert
         assert len(result) == 2
-        assert result[0].rel_path == "file_1.txt"
-        assert result[1].rel_path == "file_3.txt"
+        assert result[0] == self.file_1
+        assert result[1] == self.file_3
 
     def test_are_the_same_returns_true_when_objects_represent_the_same_file(self):
         # Arrange
@@ -82,9 +82,28 @@ class TestSorageDifferenceProcessor:
 
     def test_are_the_same_returns_false_when_objects_represent_different_files(self):
         # Arrange
-        s3_object = self.s3_object_3
+        s3_object = self.s3_object_2
         file_information = self.file_1
         # Act
         result = self.processor.are_the_same(s3_object, file_information)
         # Assert
         assert result is False
+
+    def test_are_the_same_returns_true_when_objects_represent_the_same_file_but_with_windows_path(self):
+        # Arrange
+        s3_object = self.s3_object_3
+        file_information = self.file_3
+        # Act
+        result = self.processor.are_the_same(s3_object, file_information)
+        # Assert
+        assert result is True
+
+    def test_are_the_same_returns_true_when_objects_represent_the_same_file_but_with_mixed_path(self):
+        # Arrange
+        s3_object = self.s3_object_4
+        file_information = self.file_4
+        # Act
+        result = self.processor.are_the_same(s3_object, file_information)
+        # Assert
+        assert result is True
+
