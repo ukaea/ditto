16c1d67824a6c0e4627c5c74a1b1ec3a93670b15
Oliver Tarrant <oliver.tarrant@tessella.com>
2018-11-14 11:59:24 +0000

    Adapt return_new_files to have capacity to check for updated files

--- ditto_web_api/DittoWebApi/src/services/data_replication/storage_difference_processor.py
+++ ditto_web_api/DittoWebApi/src/services/data_replication/storage_difference_processor.py
@@ -4,8 +4,9 @@ import os
 
 class StorageDifferenceProcessor:
 
-    def return_new_files(self, objects_in_bucket, files_in_directory):
+    def return_new_files(self, objects_in_bucket, files_in_directory, check_for_updates=False):
         list_of_new_files = []
+        files_to_update = []
         if not objects_in_bucket:
             return files_in_directory
         objects_to_check = [s3_obj for s3_obj in objects_in_bucket]
@@ -17,17 +18,15 @@ class StorageDifferenceProcessor:
                 if not any(matches):
                     list_of_new_files.append(file_information)
                 else:
+                    if check_for_updates:
+                        if self.need_update(objects_to_check[matches.index(True)], file_information):
+                            files_to_update.append(file_information)
                     del objects_to_check[matches.index(True)]
-        return list_of_new_files
+        return list_of_new_files, files_to_update
 
     def are_the_same(self, s3_object, file_information):
         s3_object_name = s3_object.object_name
         return file_information.rel_path == s3_object_name
 
-
-    # Edit return new files so when a match is found this new function is called and comparison is made
-    # return true if the file needs upgrading else return false
     def need_update(self, s3_object, file_information):
         return s3_object.last_modified < os.stat(file_information.abs_path).st_mtime
-
-
