4a132823766f7e81096fd04bb30ba00c2d25c506
Oliver Tarrant <oliver.tarrant@tessella.com>
2018-11-15 09:41:34 +0000

    More extensive checks for path helpers

--- ditto_web_api/DittoWebApi/tests/utils/path_helpers_test.py
+++ ditto_web_api/DittoWebApi/tests/utils/path_helpers_test.py
@@ -3,17 +3,25 @@ from DittoWebApi.src.utils.file_system.path_helpers import to_posix
 
 
 class TestPathHelpers(unittest.TestCase):
-    def test_windows_path_converted_to_unix(self):
+    def test_windows_path_converted_to_posix(self):
         path = r'c\test\subdir\file.txt'
         self.assertEqual(to_posix(path), r"c/test/subdir/file.txt")
 
-    def test_mixed_paths_converted_to_linux_paths(self):
+    def test_mixed_path_converted_to_posix(self):
+        path = r"c/test\subdir/file.txt"
+        self.assertEqual(to_posix(path), r"c/test/subdir/file.txt")
+
+    def test_posix_path_remain_posix(self):
         path = r"c/test/subdir/file.txt"
         self.assertEqual(to_posix(path), r"c/test/subdir/file.txt")
 
-    def test_linux_paths_remain_linux_paths(self):
-        path = r"c/test/subdir/file.txt"
-        self.assertEqual(to_posix(path), r"c/test/subdir/file.txt")
+    def test_windows_directory_converts_to_posix_final_slash_removed(self):
+        path = r"C:\\test\\subdir\\"
+        self.assertEqual(to_posix(path), r"C:/test/subdir")
+
+    def test_posix_directory_remain_posix_final_slash_removed(self):
+        path = r"C:/test/subdir/"
+        self.assertEqual(to_posix(path), r"C:/test/subdir")
 
     def test_blank_string_remains_empty(self):
         path = r" "
