a624f3e9350c3678694b663cbe1573e4b8762e2c
Oliver Tarrant <oliver.tarrant@tessella.com>
2018-11-21 10:56:56 +0000

    Extra test for bucket name validation

--- ditto_web_api/DittoWebApi/tests/utils/bucket_helper_test.py
+++ ditto_web_api/DittoWebApi/tests/utils/bucket_helper_test.py
@@ -3,7 +3,13 @@ from DittoWebApi.src.utils.bucket_helper import is_valid_bucket
 
 
 class TestBucketHelpers:
-    @pytest.mark.parametrize("bucket_name", ["test-1234", "test-1234", "tes", "test123--12", "test.test", "123test"])
+    @pytest.mark.parametrize("bucket_name", ["test-1234",
+                                             "test-1234",
+                                             "tes",
+                                             "test123--12",
+                                             "test.test",
+                                             "123test",
+                                             "a-test-that-is-just-as-long-as-is-allowed-here-with-the-limit63"])
     def test_is_valid_returns_true_for_s3_bucket_names_that_are_valid(self, bucket_name):
         # Arrange
         name_to_test = bucket_name
