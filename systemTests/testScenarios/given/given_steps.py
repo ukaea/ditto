f1fcda78aa949d39169e0f31ff0c34d4de7d95ea
Robert Clegg <Robert.Clegg@tessella.com>
2018-11-21 15:54:01 +0000

    Tidy systemTests given_steps.py

--- systemTests/testScenarios/given/given_steps.py
+++ systemTests/testScenarios/given/given_steps.py
@@ -13,12 +13,8 @@ class GivenSteps:
         return self._ditto_api_server
 
     def _write_test_file(self, file_name, content):
-        with open(os.path.join(self._context.execution_folder_path, 'testing_area/src', file_name), 'w') as file:
+        with open(os.path.join(self._context.local_data_folder_path, file_name), 'w') as file:
             file.write(content)
 
     def simple_test_file_is_setup(self):
         self._write_test_file('testA.txt', 'example test content A')
-
-    def a_file_in_src(self):
-        self._write_test_file('testPermissions.txt', 'this is a text file for testing GET permissions')
-        return self._file_in_src
