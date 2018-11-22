34e3698901602bcc69d7dfcdd7dd5f0b0a65326d
Robert Clegg <Robert.Clegg@tessella.com>
2018-11-22 10:45:24 +0000

    Fix system test post request

--- systemTests/testScenarios/when/when_steps.py
+++ systemTests/testScenarios/when/when_steps.py
@@ -12,9 +12,9 @@ class WhenSteps:
         print(self.__class__)
 
     def create_bucket_called_for_simple_bucket(self):
-        url = f'http://{self._context.s3host}:{self._context.s3port}/createbucket/'
+        url = f'http://{self._context.s3host}:{self._context.app_port}/createbucket/'
         body = {'bucket': 'systemtest-textbucket'}
         print(url)
-        response = requests.post(url, data=body)
+        response = requests.post(url, json=body)
         print(response.status_code)
         return response
