93ea94122c6d70aa21c7ad881039268488e295b4
Robert Clegg <Robert.Clegg@tessella.com>
2018-11-26 16:28:38 +0000

    Fix flake8 error in str2list_tests

--- ditto_web_api/DittoWebApi/tests/utils/str2list_tests.py
+++ ditto_web_api/DittoWebApi/tests/utils/str2list_tests.py
@@ -7,4 +7,3 @@ from DittoWebApi.src.utils.parse_strings import str2list
 def test_str2list_splits_any_padded_string_with_default_settings(string):
     result = str2list(string)
     assert result == ["a", "1", "foobar"]
-
