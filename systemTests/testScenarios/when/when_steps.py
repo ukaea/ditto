3d8cb4c21335ebed3a609b950eb2cf262634fc30
robert-clegg-tessella <Robert.Clegg@tessella.com>
2018-11-29 09:01:18 +0000

    System tests print port usage when request fails

--- systemTests/testScenarios/when/when_steps.py
+++ systemTests/testScenarios/when/when_steps.py
@@ -1,7 +1,7 @@
 import requests
-
 from requests.auth import HTTPBasicAuth
 
+from testScenarios.tools.port_helper import print_port_usage
 
 # Standard request timeout, in seconds
 TIMEOUT = 5
@@ -118,18 +118,33 @@ class WhenSteps:
         url = f'http://{self._context.host_address}:{self._context.app_port}/{handler}/'
         authentication = HTTPBasicAuth(self._context.authentication_username, self._context.authentication_password)
         method = "DELETE" if handler == "deletefile" else "POST"
-        response = requests.request(method, url, json=body, auth=authentication, timeout=TIMEOUT)
-        self._context.http_client_response = response
+        self._context.http_client_response = None
+        try:
+            response = requests.request(method, url, json=body, auth=authentication, timeout=TIMEOUT)
+            self._context.http_client_response = response
+        except Exception as exception:
+            print_port_usage(self._context.host_address, self._context.app_port)
+            print(exception)
 
     def _make_unauthorised_request(self, handler, body):
         url = f'http://{self._context.host_address}:{self._context.app_port}/{handler}/'
         authentication = HTTPBasicAuth('unknown_user', 'password')
         method = "DELETE" if handler == "deletefile" else "POST"
-        response = requests.request(method, url, json=body, auth=authentication, timeout=TIMEOUT)
-        self._context.http_client_response = response
+        self._context.http_client_response = None
+        try:
+            response = requests.request(method, url, json=body, auth=authentication, timeout=TIMEOUT)
+            self._context.http_client_response = response
+        except Exception as exception:
+            print_port_usage(self._context.host_address, self._context.app_port)
+            print(exception)
 
     def _make_request_with_no_authorisation_credentials(self, handler, body):
         url = f'http://{self._context.host_address}:{self._context.app_port}/{handler}/'
         method = "DELETE" if handler == "deletefile" else "POST"
-        response = requests.request(method, url, json=body)
-        self._context.http_client_response = response
+        self._context.http_client_response = None
+        try:
+            response = requests.request(method, url, json=body, timeout=TIMEOUT)
+            self._context.http_client_response = response
+        except Exception as exception:
+            print_port_usage(self._context.host_address, self._context.app_port)
+            print(exception)
