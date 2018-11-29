673b92e97707de4548d987f5d35612fa0ac563b5
robert-clegg-tessella <Robert.Clegg@tessella.com>
2018-11-29 09:52:12 +0000

    Link to nmap status meanings

--- systemTests/testScenarios/tools/port_helper.py
+++ systemTests/testScenarios/tools/port_helper.py
@@ -9,6 +9,7 @@ def is_port_in_use(address, port):
 
 
 def print_port_state(address, port):
+    # See https://nmap.org/book/man-port-scanning-basics.html for an explanation of the various states
     scanner = nmap.PortScanner()
     scanner.scan(str(address), str(port))
     state = scanner[str(address)]['tcp'][int(port)]['state']
