4b95694156b3aa8dcc271190b5883af54f65cd27
Robert Clegg <robert.clegg@tessella.com>
2018-11-30 17:52:02 +0000

    Fix mistakes in cretae_bucket_when_steps.py

--- systemTests/testScenarios/when/create_bucket_when_steps.py
+++ systemTests/testScenarios/when/create_bucket_when_steps.py
@@ -7,10 +7,15 @@ class CreateBucketWhenSteps(BaseWhenStep):
         body = {'bucket': self._context.standard_bucket_name}
         self._make_authorised_request(handler, body)
 
-    def unauthenticated_create_bucket_called_for_simple_bucket(self):
+    def unauthorised_create_bucket_called_for_simple_bucket(self):
         handler = 'createbucket'
         body = {'bucket': self._context.standard_bucket_name}
-        self._make_unauthenticated_request(handler, body)
+        self._make_unauthorised_request(handler, body)
+
+    def create_bucket_called_for_simple_bucket_with_no_authorisation_credentials(self):
+        handler = 'createbucket'
+        body = {'bucket': self._context.standard_bucket_name}
+        self._make_request_with_no_authorisation_credentials(handler, body)
 
     def authorised_create_bucket_called_with_name(self, name):
         handler = 'createbucket'
