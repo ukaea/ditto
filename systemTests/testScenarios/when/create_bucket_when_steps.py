5f43a76c345796ea8d42423b6776901be279f89d
robert-clegg-tessella <Robert.Clegg@tessella.com>
2018-12-04 10:12:35 +0000

    Clarify createbucket system tests

--- systemTests/testScenarios/when/create_bucket_when_steps.py
+++ systemTests/testScenarios/when/create_bucket_when_steps.py
@@ -2,22 +2,22 @@ from testScenarios.when.base_when_step import BaseWhenStep
 
 
 class CreateBucketWhenSteps(BaseWhenStep):
-    def authorised_create_bucket_called_for_simple_bucket(self):
+    def authenticated_create_bucket_called_for_simple_bucket(self):
         handler = 'createbucket'
         body = {'bucket': self._context.standard_bucket_name}
         self._make_authorised_request(handler, body)
 
-    def unauthorised_create_bucket_called_for_simple_bucket(self):
+    def unauthenticated_create_bucket_called_for_simple_bucket(self):
         handler = 'createbucket'
         body = {'bucket': self._context.standard_bucket_name}
-        self._make_unauthorised_request(handler, body)
+        self._make_unauthenticated_request(handler, body)
 
     def create_bucket_called_for_simple_bucket_with_no_user_credentials(self):
         handler = 'createbucket'
         body = {'bucket': self._context.standard_bucket_name}
         self._make_request_with_no_user_credentials(handler, body)
 
-    def authorised_create_bucket_called_with_name(self, name):
+    def authenticated_create_bucket_called_with_name(self, name):
         handler = 'createbucket'
         body = {'bucket': name}
         self._make_authorised_request(handler, body)
