bcee0efc9ec5a83dd9be22cb599331d9580db214
Oliver Tarrant <oliver.tarrant@tessella.com>
2018-12-05 15:06:13 +0000

    Fix return helper unit tests

--- ditto_web_api/DittoWebApi/tests/utils/return_helper_test.py
+++ ditto_web_api/DittoWebApi/tests/utils/return_helper_test.py
@@ -1,5 +1,6 @@
 import unittest
 from DittoWebApi.src.utils.return_helper import return_transfer_summary
+from DittoWebApi.src.utils.return_status import StatusCodes
 
 
 class TestReturnHelper(unittest.TestCase):
@@ -9,23 +10,27 @@ class TestReturnHelper(unittest.TestCase):
                                             files_skipped=2,
                                             files_updated=4,
                                             data_transferred=102,
-                                            message=message)
+                                            message=message,
+                                            status=StatusCodes.Okay)
         self.assertEqual(test_dict, {"message": message,
                                      "new files uploaded": 5,
                                      "files updated": 4,
                                      "files skipped": 2,
-                                     "data transferred (bytes)": 102})
+                                     "data transferred (bytes)": 102,
+                                     "status": StatusCodes.Okay})
 
     def test_correct_return_when__no_message_is_added_for_return_dict(self):
         test_dict = return_transfer_summary(new_files_uploaded=6,
                                             files_skipped=3,
                                             files_updated=1,
-                                            data_transferred=2532, )
+                                            data_transferred=2532,
+                                            status=StatusCodes.Okay)
         self.assertEqual(test_dict, {"message": "",
                                      "new files uploaded": 6,
                                      "files updated": 1,
                                      "files skipped": 3,
-                                     "data transferred (bytes)": 2532})
+                                     "data transferred (bytes)": 2532,
+                                     "status": StatusCodes.Okay})
 
     def test_default_dict_returned_when_return_dict_called_with_no_args(self):
         test_dict = return_transfer_summary()
@@ -33,4 +38,5 @@ class TestReturnHelper(unittest.TestCase):
                                      "new files uploaded": 0,
                                      "files updated": 0,
                                      "files skipped": 0,
-                                     "data transferred (bytes)": 0})
+                                     "data transferred (bytes)": 0,
+                                     "status": StatusCodes.Okay})
