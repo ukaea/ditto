6d59f20cb3f448ef03a15a34ec256e0639a2af09
Oliver Tarrant <oliver.tarrant@tessella.com>
2018-12-06 10:27:33 +0000

    add .- and -. to invalid items in bucket name

--- ditto_web_api/DittoWebApi/src/utils/bucket_helper.py
+++ ditto_web_api/DittoWebApi/src/utils/bucket_helper.py
@@ -4,7 +4,8 @@ import re
 def is_valid_bucket(bucket_name):
     if len(bucket_name) < 3 or len(bucket_name) > 63:
         return False
-    if '..' in bucket_name:
+    invalids = ('..', '.-', '-.')
+    if any(invalid_string in bucket_name for invalid_string in invalids):
         return False
     match = re.compile('^[a-z0-9][a-z0-9\\.\\-]+[a-z0-9]$').match(bucket_name)
     if match is None or match.end() != len(bucket_name):
