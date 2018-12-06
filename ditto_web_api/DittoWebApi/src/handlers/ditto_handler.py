05d9a358271edf6f9a934a7f2e27b1cac653bb71
Oliver Tarrant <oliver.tarrant@tessella.com>
2018-12-06 10:32:22 +0000

    Revert message for when buck name not found

--- ditto_web_api/DittoWebApi/src/handlers/ditto_handler.py
+++ ditto_web_api/DittoWebApi/src/handlers/ditto_handler.py
@@ -24,7 +24,7 @@ class DittoHandler(APIHandler):
 
     def check_current_user_authorised_for_bucket(self, bucket_name):
         if not self._bucket_settings_service.is_bucket_recognised(bucket_name):
-            raise exceptions.APIError(404, 'Bucket does not exist')
+            raise exceptions.APIError(404, 'Bucket name not recognised')
         permitted_groups = self._bucket_settings_service.bucket_permitted_groups(bucket_name)
         for group_name in permitted_groups:
             if self._security_service.is_in_group(self._current_user, group_name):
