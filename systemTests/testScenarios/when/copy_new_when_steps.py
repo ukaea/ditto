9f4d98985d5881ef84c6dc754ea6bb3d9a94a4a7
Oliver Tarrant <oliver.tarrant@tessella.com>
2018-12-06 15:15:58 +0000

    Add when steps for calls out of root

--- systemTests/testScenarios/when/copy_new_when_steps.py
+++ systemTests/testScenarios/when/copy_new_when_steps.py
@@ -21,3 +21,8 @@ class CopyNewWhenSteps(BaseWhenStep):
         handler = 'copynew'
         body = {'bucket': self._context.standard_bucket_name}
         self._make_request_with_no_user_credentials(handler, body)
+
+    def authorised_copy_new_called_for_directory_up_from_root(self):
+        handler = 'copynew'
+        body = {'bucket': self._context.standard_bucket_name, 'directory': '../data2'}
+        self._make_authorised_request(handler, body)
