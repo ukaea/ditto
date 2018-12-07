883fe48b271dc1f72ee4af6356943ea3a373a726
Robert Clegg <robert.clegg@tessella.com>
2018-12-07 14:44:14 +0000

    Reorder files_system_helper alphabetically

--- ditto_web_api/DittoWebApi/src/utils/file_system/files_system_helpers.py
+++ ditto_web_api/DittoWebApi/src/utils/file_system/files_system_helpers.py
@@ -3,8 +3,27 @@ import os
 
 
 class FileSystemHelper:
-    def join_paths(self, directory, file_name):
-        return os.path.join(directory, file_name)
+    def absolute_file_path(self, file_path):
+        return os.path.abspath(file_path)
+
+    def close_file(self, open_file):
+        if not open_file.closed:
+            open_file.close()
+
+    def create_and_open_file_for_writing(self, file_path):
+        return open(file_path, "w")
+
+    def does_file_exist(self, file_path):
+        return os.path.exists(file_path)
+
+    def file_directory(self, file_rel_path):
+        return os.path.dirname(file_rel_path)
+
+    def file_name(self, abs_file_path):
+        return os.path.basename(abs_file_path)
+
+    def file_size(self, abs_file_path):
+        return os.stat(abs_file_path).st_size
 
     def find_all_files_in_folder(self, directory):
         list_of_files = []
@@ -13,33 +32,14 @@ class FileSystemHelper:
                 list_of_files.append(self.join_paths(path, name))
         return list_of_files
 
-    def absolute_file_path(self, file_path):
-        return os.path.abspath(file_path)
-
-    def relative_file_path(self, file_path, root_directory):
-        return os.path.relpath(self.absolute_file_path(file_path), root_directory)
-
-    def file_name(self, abs_file_path):
-        return os.path.basename(abs_file_path)
+    def join_paths(self, directory, file_name):
+        return os.path.join(directory, file_name)
 
     def last_modified(self, file_path):
         return os.path.getmtime(file_path)
 
-    def file_size(self, abs_file_path):
-        return os.stat(abs_file_path).st_size
-
-    def create_and_open_file_for_writing(self, file_path):
-        return open(file_path, "w")
-
-    def does_file_exist(self, file_path):
-        return os.path.exists(file_path)
-
     def open_file_for_reading_and_writing(self, file_path):
         return open(file_path, 'r+')
 
-    def close_file(self, open_file):
-        if not open_file.closed:
-            open_file.close()
-
-    def file_directory(self, file_rel_path):
-        return os.path.dirname(file_rel_path)
+    def relative_file_path(self, file_path, root_directory):
+        return os.path.relpath(self.absolute_file_path(file_path), root_directory)
