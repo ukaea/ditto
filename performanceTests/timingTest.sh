97190bc99feaeed8eaa96c21f8ee5d1dfff11ecd
Robert Clegg <robert.clegg@tessella.com>
2018-12-12 13:30:31 +0000

    Timing test wrapped in function

--- performanceTests/timingTest.sh
+++ performanceTests/timingTest.sh
@@ -12,22 +12,28 @@ EOF
 USER=$1
 PASSWORD=$2
 
-# unix time in nanoseconds
-T0=$(date +%s%N)
+function runUploadTest {
+  # unix time in nanoseconds
+  T0=$(date +%s%N)
 
-curl -i \
--H "Accept: application/json" \
--H "Content-Type:application/json" \
--X POST \
---user "${USER}:${PASSWORD}" \
---data "$(generate_post_data)" \
-"http://172.28.129.160:8888/copydir/"
+  #curl -i \
+  #-H "Accept: application/json" \
+  #-H "Content-Type:application/json" \
+  #-X POST \
+  #--user "${USER}:${PASSWORD}" \
+  #--data "$(generate_post_data)" \
+  #"http://172.28.129.160:8888/copydir/"
 
-# unix time in nanoseconds
-T1=$(date +%s%N)
+  # unix time in nanoseconds
+  T1=$(date +%s%N)
 
-# time difference in milliseconds
-T=$(($(($T1 - $T0))/1000000))
+  # time difference in milliseconds
+  T=$(($(($T1 - $T0))/1000000))
+  
+  echo "$T"
+}
+
+T=`runUploadTest`
 
 echo ""
 echo "Took $T milliseconds to upload"
