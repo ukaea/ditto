77ae4ac3c60486bb243abc36cb566a27b43908ae
Robert Clegg <robert.clegg@tessella.com>
2018-12-12 16:58:06 +0000

    Correct more performancer test labels

--- performanceTests/generateRandomFile.sh
+++ performanceTests/generateRandomFile.sh
@@ -16,15 +16,15 @@ function generateFile {
 }
 
 
-generateFile "${rootdir}/single10mb" "16MB.bin" 16M 1
-generateFile "${rootdir}/single20mb" "32MB.bin" 32M 1
-generateFile "${rootdir}/single50mb" "64MB.bin" 64M 1
-generateFile "${rootdir}/single100mb" "124MB.bin" 64M 2
-generateFile "${rootdir}/single200mb" "256MB.bin" 64M 4
-generateFile "${rootdir}/single500mb" "512MB.bin" 64M 8
+generateFile "${rootdir}/single16mb" "16MB.bin" 16M 1
+generateFile "${rootdir}/single32mb" "32MB.bin" 32M 1
+generateFile "${rootdir}/single64mb" "64MB.bin" 64M 1
+generateFile "${rootdir}/single128mb" "124MB.bin" 64M 2
+generateFile "${rootdir}/single256mb" "256MB.bin" 64M 4
+generateFile "${rootdir}/single512mb" "512MB.bin" 64M 8
 generateFile "${rootdir}/single1gb" "1GB.bin" 64M 16
 generateFile "${rootdir}/single2gb" "2GB.bin" 64M 32
 generateFile "${rootdir}/single4gb" "4GB.bin" 64M 64
 for n in {1..8}; do {
-  generateFile "${rootdir}/eight50mb" "1GB_${n}.bin" 64M 1
+  generateFile "${rootdir}/eight64mb" "64mb_${n}.bin" 64M 1
 } done
